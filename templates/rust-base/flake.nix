25964fcf27b5c7bdd012703f05d3d572af787b9c
Alexander Milchinskiy <milchinskiy@gmail.com>
2025-10-28 00:06:28 +0200

    minor fix

--- templates/rust-base/flake.nix
+++ templates/rust-base/flake.nix
@@ -2,7 +2,7 @@
   description = "A basic rust project";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
   };
 
   outputs = {
